afe8bf15c4a86ea526d0c2a891b4235426f316ce
Lars Kvn <83087654+larskvn@users.noreply.github.com>
2024-10-18 10:56:15 -0500

    Testing (#5)
    * Descripción de los cambios realizados
    
    * Development (#3)
    
    * Add GitHub Actions workflow for Maven project
    
    * add service and controller
    
    * add service and controller++
    
    * Update ci.yml
    
    * Development (#4)
    
    * Add GitHub Actions workflow for Maven project
    
    * add service and controller
    
    * add service and controller++
    
    * tes and update action
    
    * tes and update action++
    
    * tes and update action+
    
    * tes and update config
    
    * tes and update config 2
    
    * test
    
    * update meta data
    
    * test

--- src/main/java/com/aplication/rest/repositorio/ProductoRepository.java
+++ src/main/java/com/aplication/rest/repositorio/ProductoRepository.java
@@ -4,6 +4,5 @@ import com.aplication.rest.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ProductoRepository  extends JpaRepository<Producto, Long> {
+
 }
